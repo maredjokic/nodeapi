0441ac607e7190b1163d3478086fd01cc719b4b0
Marko Djokic <mare160696@gmail.com>
2025-08-05 13:04:16 +0200

    congif and user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,19 @@
-import { Schema, model } from 'mongoose';
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-const UserSchema = new Schema({
-  name: String,
-  email: String
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
-export default model('User', UserSchema);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = function (enteredPassword: string) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
